53773aa50fc0d8951a78194295161bcbaa53326c
Anurag <anuragsinghg99@gmail.com>
2022-11-07 21:03:21 +0530

    special thanks page created

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,24 @@
-import Document, {  Html, Head, Main, NextScript } from "next/document";
- import Script from "next/script";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html >
+      <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
-        <body className="bg-gray-800 ">
-        <Script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"  strategy='beforeInteractive'/>
-          <Script src="https://cdn.tailwindcss.com" strategy='beforeInteractive' />
+        <body className="bg-gray-800  ">
+          <Script
+            src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"
+            strategy="beforeInteractive"
+          />
+          <Script
+            src="https://cdn.tailwindcss.com"
+            strategy="beforeInteractive"
+          />
           <Main />
           <NextScript />
         </body>
