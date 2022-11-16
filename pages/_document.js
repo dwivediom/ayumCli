ceb525dd4725b701896d32217a3268675698f8b7
Anurag <anuragsinghg99@gmail.com>
2022-11-16 12:59:29 +0530

    tried to solve deployment issues

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,17 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
-        <body className="bg-gray-800   ">
-          <div>
-            <Main />
-            <NextScript />
-          </div>
+        <body className="bg-gray-800  ">
+          <Script
+            src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"
+            strategy="beforeInteractive"
+          />
+          <Script
+            src="https://cdn.tailwindcss.com"
+            strategy="beforeInteractive"
+          />
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
