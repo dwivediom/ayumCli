264024c6a3f6dbc7e8849c854862fd25b284fd7d
Anurag <anuragsinghg99@gmail.com>
2022-12-07 16:09:49 +0530

    added ssr functionality in land page

--- components/GetDoctor.js
+++ components/GetDoctor.js
@@ -6,54 +6,53 @@ import styles from "../styles/loader.module.css";
 const Doctor = dynamic(() => import("./Doctor"));
 import Image from "next/image";
 
-const GetDoctor = () => {
-  const dispatch = useDispatch();
+const GetDoctor = ({ getDoctor }) => {
+  // const dispatch = useDispatch();
 
-  const getDoctor = useSelector((state) => state.getDoctorReducer);
+  // const getDoctor = useSelector((state) => state.getDoctorReducer);
 
-  useEffect(() => {
-    dispatch(getallDoctorAction());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getallDoctorAction());
+  // }, [dispatch]);
 
-  if (getDoctor.loading) {
-    return (
-      <>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "2rem",
-          }}
-        >
-          <div className={styles.dotsbars4}></div>
-        </div>
-      </>
-    );
-  }
+  // if (getDoctor.loading) {
+  //   return (
+  //     <>
+  //       <div
+  //         style={{
+  //           width: "100%",
+  //           display: "flex",
+  //           justifyContent: "center",
+  //           marginTop: "2rem",
+  //         }}
+  //       >
+  //         <div className={styles.dotsbars4}></div>
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
       <div className="  md:grid m-auto  overflow-hidden md:grid-cols-2  lg:grid-cols-4  gap-3">
         {/* <div className={`${styles.doc_container}`} > */}
-        {getDoctor.doctor &&
-          getDoctor.doctor
-            .slice(0)
-            .reverse()
-            .map((doctor) => {
-              return (
-                <Doctor
-                  key={doctor._id}
-                  name={doctor.doctor.name}
-                  specialist={doctor.specialist}
-                  location={doctor.location}
-                  phone={doctor.doctor.phone}
-                  fees={doctor.fees}
-                  timing={doctor.timing}
-                  docid={doctor.doctor._id}
-                />
-              );
-            })}
+        {getDoctor
+          .slice(0)
+          .reverse()
+          .map((doctor) => {
+            return (
+              <Doctor
+                key={doctor._id}
+                name={doctor.doctor.name}
+                specialist={doctor.specialist}
+                location={doctor.location}
+                phone={doctor.doctor.phone}
+                fees={doctor.fees}
+                timing={doctor.timing}
+                docid={doctor.doctor._id}
+              />
+            );
+          })}
       </div>
     </>
   );
