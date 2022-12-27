ce6473fe44ec0f1ea51d55a5b85dd8f93df3505d
Anurag <anuragsinghg99@gmail.com>
2022-12-27 10:31:02 +0530

    solved aboutus errors

--- pages/About.jsx
+++ pages/About.jsx
@@ -5,6 +5,16 @@ import Aboutus from "../components/Aboutus";
 const About = () => {
   return (
     <>
+      <Head>
+        <title>About Us- Ayum</title>
+        <meta name="title" content="About Us- Ayum " />
+        <meta
+          name="description"
+          content="Ayum is an One Stop Online platform that provides the facility
+          to Book Appointments and using Ayum you can also get your Lab
+          tests report in your smartphone..."
+        />
+      </Head>
       <Aboutus />
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -15,25 +15,6 @@ function Loading() {
 
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const handleStart = (url) => url !== router.asPath && setLoading(true);
-  //   const handleComplete = (url) =>
-  //     url === router.asPath &&
-  //     setTimeout(() => {
-  //       setLoading(false);
-  //     }, 2500);
-
-  //   router.events.on("routeChangeStart", handleStart);
-  //   router.events.on("routeChangeComplete", handleComplete);
-  //   router.events.on("routeChangeError", handleComplete);
-
-  //   return () => {
-  //     router.events.off("routeChangeStart", handleStart);
-  //     router.events.off("routeChangeComplete", handleComplete);
-  //     router.events.off("routeChangeError", handleComplete);
-  //   };
-  // });
-
   useEffect(() => {
     const handleRouteChange = () => {
       setLoading(true);
