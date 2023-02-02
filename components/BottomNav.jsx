0c8d1a2062e10e7465f83d4878d9ea9ded4b56ad
Anurag <anuragsinghg99@gmail.com>
2023-02-02 12:55:03 +0530

    solved some issues

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -140,24 +140,23 @@ const BottomNav = () => {
               </a>
             </Link>
             <Link href={"/Doctor/DocAppointment"}>
-              <a className="w-full focus:text-cyan-500 hover:text-cyan-500 justify-center inline-block text-center pt-2 pb-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className=" text-center inline w-6  h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
-                  />
-                </svg>
+              <a
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  gap: "3px",
+                }}
+                className="w-full focus:text-cyan-500 hover:text-cyan-500 justify-center inline-block text-center pt-2 pb-1"
+              >
+                <img
+                  style={{ width: "25px", height: "25px" }}
+                  src="https://img.icons8.com/external-vectorslab-glyph-vectorslab/53/FFFFFF/external-Injection-medical-and-corona-virus-vectorslab-glyph-vectorslab.png"
+                />
 
                 <span className="tab tab-whishlist  block text-xs">
-                  Doc Ap.
+                  Lab Tests
                 </span>
               </a>
             </Link>
