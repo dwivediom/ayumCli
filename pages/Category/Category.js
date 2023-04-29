df3a5f0406c7cc4ddf9ef0b9ad4a5e17d67e4852
Anurag <anuragsinghg99@gmail.com>
2023-04-29 18:47:54 +0530

    solved some bugs and added an upated slider

--- pages/Category/Category.js
+++ pages/Category/Category.js
@@ -1,12 +1,14 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import React from "react";
+import Slider2 from "../../components/AdComp3";
 const QuickSearch = dynamic(() => import("../../components/QuickSearch"));
 
 const Category = () => {
   return (
     <div className="absolute w-full">
       <QuickSearch />
+      <Slider2 />
     </div>
   );
 };
