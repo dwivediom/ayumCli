16ffa639545a2d8b2cef14342fa0ddb39610a6ab
Anurag1CA <anurag@onechanneladmin.com>
2024-09-23 01:06:50 +0530

    bug fixed

---
+++ components/CityDropdown.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import styles from "../styles/Home.module.css";
+
+const CityDropdown = (props) => {
+  const cityoptions = [
+    { label: "Rewa" },
+    { label: "Jabalpur" },
+    { label: "Indore" },
+    { label: "Katni" },
+    { label: "Lucknow" },
+    { label: "Satna" },
+    { label: "Sidhi" },
+    { label: "Nagpur" },
+    { label: "Gwalior" },
+  ];
+  const [value, setvalue] = useState();
+  useEffect(() => {
+    if (typeof window != "undefined") {
+      let temp = window.localStorage.getItem("city");
+      setvalue({ label: temp });
+    }
+  }, []);
+  return (
+    <Autocomplete
+      disablePortal
+      className={styles.autocomp}
+      id="combo-box-demo"
+      options={cityoptions}
+      value={value}
+      onChange={(e, val) => {
+        console.log(val);
+        typeof window != "undefined" &&
+          window.localStorage.setItem("city", val?.label);
+        setvalue({ label: val?.label });
+        props?.getdocs && props?.getdocs();
+      }}
+      renderInput={(params) => (
+        <TextField
+          placeholder={value?.label ? value?.label : "Choose city"}
+          {...params}
+          label=""
+        />
+      )}
+    />
+  );
+};
+
+export default CityDropdown;
