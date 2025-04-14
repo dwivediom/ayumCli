0a4edfd0d76d6fdbee1ece2d7473bbd36b78473e
om dhar dwivedi <omdhardwivedi@192.168.1.5>
2025-04-14 12:18:06 +0530

    comment added

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -14,6 +14,7 @@ const UserProfile = () => {
   const [appointment, setappointment] = useState("");
   const [profilepayload, setprofilepayload] = useState({});
   const [loading, setloading] = useState(false);
+  // api to get user appointments
   const Getuserappointments = async () => {
     try {
       setloading(true);
