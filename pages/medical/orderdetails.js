802cba832a5111cd2f80fe8e5fe10afb38fb4f2f
Anurag1CA <anurag@onechanneladmin.com>
2025-06-18 12:42:15 +0530

    bug fixes

--- pages/medical/orderdetails.js
+++ pages/medical/orderdetails.js
@@ -7,12 +7,13 @@ const orderdetails = () => {
   const router = useRouter();
   const { id } = router.query;
   const [invoice, setinvoice] = useState(null);
-
+  const [store, setstore] = useState(null);
   const GetOrderDetails = async () => {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_B_PORT}/api/medical/user/orderdetails?id=${id}`
+      `${process.env.NEXT_PUBLIC_B_PORT}/api/medical/user/orderdetails?id=${id}&store=true`
     );
     setinvoice(response.data.data);
+    setstore(response.data.store);
     console.log(response.data.data, "invoicedata");
   };
   useEffect(() => {
@@ -22,7 +23,9 @@ const orderdetails = () => {
   }, [id]);
   return (
     <div style={{ paddingTop: "10px" }}>
-      {invoice && <MedicalInvoice invoice={invoice} showaspage={true} />}
+      {invoice && (
+        <MedicalInvoice invoice={invoice} showaspage={true} store={store} />
+      )}
     </div>
   );
 };
