23e2f6f94da9415950c37c57a350573b0f61f01b
Anurag1CA <anurag@onechanneladmin.com>
2026-01-01 18:11:26 +0530

    deploy fix

--- next.config.js
+++ next.config.js
@@ -1,24 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const runtimeCaching = require("next-pwa/cache");
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
 
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  swSrc: 'worker/service-worker.js',
-  // runtimeCaching,
+  swSrc: "worker/service-worker.js",
+  runtimeCaching,
   buildExcludes: [/middleware-manifest.json$/],
-  // disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = withPWA({
+  // 🔥 REQUIRED for Azure App Service
+  output: "standalone",
+
   images: {
     domains: [
       "www.pexels.com",
@@ -33,11 +30,15 @@ const nextConfig = withPWA({
       "www.imgbb.com",
     ],
   },
+
   reactStrictMode: true,
   swcMinify: true,
+
   env: {
+    // Client-safe env (compile-time)
     Bport: "http://localhost:5000",
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
